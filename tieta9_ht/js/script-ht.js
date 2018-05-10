2658492c3efbece27845d75f8795765ef56d3a53
Arttu Ylhävuori <arjymoi@hotmail.com>
2018-05-10 14:00:40 +0300

    viilattu nimen muuttamistoimintoa -> valmis + testi: nappien toimivuus

--- tieta9_ht/js/script-ht.js
+++ tieta9_ht/js/script-ht.js
@@ -3,12 +3,12 @@
 let nimialue = document.querySelector("#nimialue");
 let nimi = document.querySelector("#nimi");
 
-nimi.textContent = "Anna Puu";
+nimi.textContent = "Kaija Koo";
 
 let pistealue = document.querySelector("#pistealue");
 let pisteet = document.querySelector("#pisteet");
 
-pisteet.textContent = "0";
+pisteet.textContent = "0$";
 
 let ylos = document.querySelector("#ylos");
 let alas = document.querySelector("#alas");
@@ -16,13 +16,32 @@ let vasen = document.querySelector("#vasen");
 let oikea = document.querySelector("#oikea");
 
 nimialue.addEventListener('click', vaihdaNimi);
-/* ylos.addEventListener('click', ylosToiminto); */
-/* alas.addEventListener('click', alasToiminto); */
-/* vasen.addEventListener('click', vasenToiminto); */
-/* oikea.addEventListener('click', oikeaToiminto); */
+
+ylos.addEventListener('click', ylosToiminto);
+alas.addEventListener('click', alasToiminto);
+vasen.addEventListener('click', vasenToiminto);
+oikea.addEventListener('click', oikeaToiminto);
 
 function vaihdaNimi() {
-    let syote = prompt("Nimi", "Elli Esimerkki");
-    // ehto: syotteen tulee olla vähintään 2 merkkiä
-    nimi.textContent = syote;
+    let syote = prompt("Syötä uusi nimi:");
+    // ehto: syote ei saa olla tyhjä ja syotteen tulee olla vähintään 2 merkkiä
+    if (syote != null && syote.length >= 2) {
+        nimi.textContent = syote;
+    }
+}
+
+function ylosToiminto() {
+    pisteet.textContent = "ylös";
+}
+
+function alasToiminto() {
+    pisteet.textContent = "alas";
+}
+
+function vasenToiminto() {
+    pisteet.textContent = "vasen";
+}
+
+function oikeaToiminto() {
+    pisteet.textContent = "oikea";
 }
